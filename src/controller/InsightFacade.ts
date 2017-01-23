a3e877b033357f8d9c37e310109d1572ee50e41d
Haoyu Yang <yhymason@users.noreply.github.com>
2017-01-23 11:44:11 -0800

    addDataset and performQuery modified
    both should return a promise containing "code" like 200 or 204 or etc, along with actual data.

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -3,23 +3,36 @@
  */
 import {IInsightFacade, InsightResponse, QueryRequest} from "./IInsightFacade";
 
-import Log from "../Util";
+import Log from "../Util"
+let JSZip = require("jszip");
+let fs = require("fs");
 
 export default class InsightFacade implements IInsightFacade {
-
     constructor() {
         Log.trace('InsightFacadeImpl::init()');
     }
 
     addDataset(id: string, content: string): Promise<InsightResponse> {
-        return null;
+
+        let zip = new JSZip();
+        zip.loadAsync(content);
+        console.log(zip);
+        let p:Promise<InsightResponse> = null;
+        return p;
+
     }
 
     removeDataset(id: string): Promise<InsightResponse> {
         return null;
     }
 
-    performQuery(query: QueryRequest): Promise <InsightResponse> {
-        return null;
+    performQuery(query: QueryRequest): Promise<InsightResponse> {
+            fs.readFile("./courses.zip", function(err:string, data: string){
+            if (err) throw err;
+            let t:Promise<InsightResponse> = this.addDataset(course, data);
+            return t;
+
+        })
+            return null;
     }
 }
