7fc8cd1af6d88be7bddee3cc1353e84aca31cf29
Haoyu Yang <yhymason@users.noreply.github.com>
2017-01-29 03:07:00 -0800

    Working version with addDataset and removeDataset
    addDataset reads content and puts content in a "id.dat" file as JSON strings
    removeDataset removes a "id.dat" file whose name is id

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -14,25 +14,88 @@ export default class InsightFacade implements IInsightFacade {
 
     addDataset(id: string, content: string): Promise<InsightResponse> {
 
-        let zip = new JSZip();
-        zip.loadAsync(content);
-        console.log(zip);
-        let p:Promise<InsightResponse> = null;
-        return p;
+        return new Promise<InsightResponse>(function(resolve, reject) {
+            let zip = new JSZip();
+            zip.loadAsync(content).then(function (data: any) {
+                //console.log(data);
+                if(fs.existsSync("./data/"+id+".dat"))
+                {
+                    resolve({code: 201, body: {}});
+                }
+                else if(!fs.existsSync("./data"+id+".dat"))
+                {
+                    let proList: Promise<string>[] = [];
+                    let keys = Object.keys(data);
+                    let objkeys = Object.keys(data[keys[0]]);
+                    let to_write_list: string[] = [];
+                    for(let i=1; i<objkeys.length; i++)
+                    {
+                        proList.push(zip.file(objkeys[i]).async("string"));
+                    }
+                    Promise.all(proList).then(strings=>{
+                        for(let i=1; i<objkeys.length; i++)
+                        {
+                            if(strings[i-1].charAt(0)=="\"")
+                                reject({code: 400, body: {'error': "Could not parse JSON"}});
+                            let name: string = objkeys[i].split('/')[1];
+                            let s: string = "\""+name+"\""+":"+strings[i-1];
+                            s = "{" + s + "}";
+                            let parsed = JSON.parse(s);
+                            //console.log(s);
+                            to_write_list.push(parsed);
+                        }
+                        if(!fs.existsSync("./data"))
+                        {
+                            fs.mkdirSync("./data");
+                            let path:string = "./data/" + id + ".dat";
+                            fs.writeFileSync(path, JSON.stringify(to_write_list));
+                        }
+                        else
+                        {
+                            let path:string = "./data/" + id + ".dat";
+                            fs.writeFileSync(path, JSON.stringify(to_write_list));
+                        }
+                        resolve({code: 204, body: {}});
+                    });
+                }
+                else
+                    reject({code: 400, body: {'error': "Could not create file"}});
 
+
+            }).catch(function (err: any) {
+                console.log(err);
+                reject({code: 400, body: {'error': err}});
+            });
+        });
     }
 
     removeDataset(id: string): Promise<InsightResponse> {
-        return null;
+        return new Promise<InsightResponse>(function (resolve, reject) {
+            let path:string = "./data/" + id + ".dat";
+            if (!fs.existsSync("./data"))
+            {
+                console.log("in 1st if");
+                reject({code: 404, body: {}});
+            }
+            else if (!fs.existsSync(path))
+            {
+                console.log("in 2nd else if");
+                reject({code: 404, body: {}});
+            }
+            else
+            {
+                console.log("in else");
+                fs.unlinkSync(path);
+                resolve({code: 204, body: {}});
+            }
+        }).catch(function (err: any) {
+            console.log(err);
+        });
+
     }
 
     performQuery(query: QueryRequest): Promise<InsightResponse> {
-            fs.readFile("./courses.zip", function(err:string, data: string){
-            if (err) throw err;
-            let t:Promise<InsightResponse> = this.addDataset(course, data);
-            return t;
 
-        })
             return null;
     }
 }
