e2ce23da55210ee9c206b6f0bf96e86f11da642a
Haoyu Yang <yhymason@users.noreply.github.com>
2017-01-29 21:32:30 -0800

    Add files via upload

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -16,55 +16,49 @@ export default class InsightFacade implements IInsightFacade {
 
         return new Promise<InsightResponse>(function(resolve, reject) {
             let zip = new JSZip();
+            if(fs.existsSync("./data/"+id+".dat"))
+            {
+                resolve({code: 201, body: {}});
+            }
             zip.loadAsync(content).then(function (data: any) {
                 //console.log(data);
-                if(fs.existsSync("./data/"+id+".dat"))
+                let proList: Promise<string>[] = [];
+                let keys = Object.keys(data);
+                let objkeys = Object.keys(data[keys[0]]);
+                let to_write_list: string[] = [];
+                for(let i=1; i<objkeys.length; i++)
                 {
-                    resolve({code: 201, body: {}});
+                    proList.push(zip.file(objkeys[i]).async("string"));
                 }
-                else if(!fs.existsSync("./data"+id+".dat"))
-                {
-                    let proList: Promise<string>[] = [];
-                    let keys = Object.keys(data);
-                    let objkeys = Object.keys(data[keys[0]]);
-                    let to_write_list: string[] = [];
+                Promise.all(proList).then(strings=>{
                     for(let i=1; i<objkeys.length; i++)
                     {
-                        proList.push(zip.file(objkeys[i]).async("string"));
+                        if(strings[i-1].charAt(0)=="\"")
+                            reject({code: 400, body: {'error': "Could not parse JSON"}});
+                        let name: string = objkeys[i].split('/')[1];
+                        let s: string = "\""+name+"\""+":"+strings[i-1];
+                        s = "{" + s + "}";
+                        let parsed = JSON.parse(s);
+                        //console.log(s);
+                        to_write_list.push(parsed);
                     }
-                    Promise.all(proList).then(strings=>{
-                        for(let i=1; i<objkeys.length; i++)
-                        {
-                            if(strings[i-1].charAt(0)=="\"")
-                                reject({code: 400, body: {'error': "Could not parse JSON"}});
-                            let name: string = objkeys[i].split('/')[1];
-                            let s: string = "\""+name+"\""+":"+strings[i-1];
-                            s = "{" + s + "}";
-                            let parsed = JSON.parse(s);
-                            //console.log(s);
-                            to_write_list.push(parsed);
-                        }
-                        if(!fs.existsSync("./data"))
-                        {
-                            fs.mkdirSync("./data");
-                            let path:string = "./data/" + id + ".dat";
-                            fs.writeFileSync(path, JSON.stringify(to_write_list));
-                        }
-                        else
-                        {
-                            let path:string = "./data/" + id + ".dat";
-                            fs.writeFileSync(path, JSON.stringify(to_write_list));
-                        }
-                        resolve({code: 204, body: {}});
-                    });
-                }
-                else
-                    reject({code: 400, body: {'error': "Could not create file"}});
-
+                    if(fs.existsSync("./data"))
+                    {
+                        let path:string = "./data/" + id + ".dat";
+                        fs.writeFileSync(path, JSON.stringify(to_write_list));
+                    }
+                    else
+                    {
+                        fs.mkdirSync("./data");
+                        let path:string = "./data/" + id + ".dat";
+                        fs.writeFileSync(path, JSON.stringify(to_write_list));
+                    }
+                    resolve({code: 204, body: {}});
+                });
 
             }).catch(function (err: any) {
-                console.log(err);
-                reject({code: 400, body: {'error': err}});
+                //console.log(err);
+                resolve({code: 400, body: {}});
             });
         });
     }
@@ -72,15 +66,9 @@ export default class InsightFacade implements IInsightFacade {
     removeDataset(id: string): Promise<InsightResponse> {
         return new Promise<InsightResponse>(function (resolve, reject) {
             let path:string = "./data/" + id + ".dat";
-            if (!fs.existsSync("./data"))
+            if (!fs.existsSync("./data")||!fs.existsSync(path))
             {
-                console.log("in 1st if");
-                reject({code: 404, body: {}});
-            }
-            else if (!fs.existsSync(path))
-            {
-                console.log("in 2nd else if");
-                reject({code: 404, body: {}});
+                resolve({code: 404, body: {}});
             }
             else
             {
@@ -88,7 +76,7 @@ export default class InsightFacade implements IInsightFacade {
                 fs.unlinkSync(path);
                 resolve({code: 204, body: {}});
             }
-        }).catch(function (err: any) {
+        }).catch(function (err) {
             console.log(err);
         });
 
