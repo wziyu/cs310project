feb3d871c83c9a9a82e7f6191d101506abfd4294
Haoyu Yang <yhymason@users.noreply.github.com>
2017-01-30 20:10:59 -0800

    Add files via upload

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -33,11 +33,17 @@ export default class InsightFacade implements IInsightFacade {
                     for(let i=1; i<objkeys.length; i++)
                     {
                         if(strings[i-1].charAt(0)=="\"")
-                            reject({code: 400, body: {'error': "Could not parse JSON"}});
+                            return reject({code: 400, body: {'error': "Could not parse JSON"}});
                         let name: string = objkeys[i].split('/')[1];
+                        let temp = JSON.parse(strings[i-1]);
+                        let temp_keys = Object.keys(temp);
+                        //console.log(temp_keys.indexOf('result'));
+                        if(temp_keys.indexOf('result')<0||temp['result']===undefined)
+                            return reject({code: 400, body: {'error': "Invalid data inside zip file"}});
                         let s: string = "\""+name+"\""+":"+strings[i-1];
                         s = "{" + s + "}";
                         let parsed = JSON.parse(s);
+
                         to_write_list.push(parsed);
                     }
                     if(fs.existsSync("./data"))
