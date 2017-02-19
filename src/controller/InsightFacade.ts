0412af02b3df8adece8185de7b3a6dfd318b492a
Haoyu Yang <yhymason@users.noreply.github.com>
2017-02-18 18:47:02 -0800

    Add files via upload

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -4,99 +4,139 @@ import Log from "../Util"
 import {isUndefined} from "util";
 let JSZip = require("jszip");
 let fs = require("fs");
-
+const parse5 = require('parse5');
+const rq = require('request');
 export default class InsightFacade implements IInsightFacade {
     constructor() {
         Log.trace('InsightFacadeImpl::init()');
     }
 
     addDataset(id: string, content: string): Promise<InsightResponse> {
-
         return new Promise<InsightResponse>(function (resolve, reject) {
             let zip = new JSZip();
             zip.loadAsync(content, {base64: true}).then(function (data: any) {
-                let proList: Promise<string>[] = [];
-                let keys = Object.keys(data);
-                let objkeys = Object.keys(data[keys[0]]);
-                let processed_results: any = [];
-                for (let i = 1; i < objkeys.length; i++) {
-                    let file = zip.file(objkeys[i]);
-                    if (file) {
-                        proList.push(file.async("string"));
-                    }
-                }
-                Promise.all(proList).then(strings => {
+                if(id == "courses")
+                {
+                    //console.log(data);
+                    let proList: Promise<string>[] = [];
+                    let keys = Object.keys(data);
+                    let objkeys = Object.keys(data[keys[0]]);
+                    let processed_results: any = [];
                     for (let i = 1; i < objkeys.length; i++) {
-                        if (strings[i - 1].charAt(0) == "\"")
-                            return reject({code: 400, body: {'error': "Could not parse JSON"}});
-                        let temp = JSON.parse(strings[i - 1]);
-                        let temp_keys = Object.keys(temp);
-                        if (temp_keys.indexOf('result') < 0 || temp['result'] === undefined)
-                            return reject({code: 400, body: {'error': "Invalid data inside zip file"}});
-                        let results = temp['result'];
-                        let clean_input_keys =
-                            [
-                                'Subject',
-                                'Course',
-                                'Avg',
-                                'Professor',
-                                'Title',
-                                'Pass',
-                                'Fail',
-                                'Audit',
-                                'id'
-                            ];
-                        let clean_output_keys =
-                            [
-                                'courses_dept',
-                                'courses_id',
-                                'courses_avg',
-                                'courses_instructor',
-                                'courses_title',
-                                'courses_pass',
-                                'courses_fail',
-                                'courses_audit',
-                                'courses_uuid'
-                            ];
-
-                        for (let r of results) {
-                            let newo: any = {};
-                            for (let i: number = 0; i < clean_input_keys.length; i++) {
-                                newo[clean_output_keys[i]] = r[clean_input_keys[i]];
-                            }
-                            processed_results.push(newo);
+                        let file = zip.file(objkeys[i]);
+                        if (file) {
+                            proList.push(file.async("string"));
                         }
-
                     }
-                    if (processed_results.length === 0)
-                        return reject({code: 400, body: {'error': "Nothing to write"}});
-                    if (fs.existsSync("./data")) {
-                        if (fs.existsSync("./data/" + id + ".dat")) {
-                            console.log("rewriting...");
-                            fs.writeFileSync("./data/" + id + ".dat", JSON.stringify(processed_results));
-                            return resolve({code: 201, body: {}});
+
+                    Promise.all(proList).then(strings => {
+                        for (let i = 1; i < objkeys.length; i++) {
+                            if (strings[i - 1].charAt(0) == "\"")
+                                return reject({code: 400, body: {'error': "Could not parse JSON"}});
+                            let temp = JSON.parse(strings[i - 1]);
+                            let temp_keys = Object.keys(temp);
+                            if (temp_keys.indexOf('result') < 0 || temp['result'] === undefined)
+                                return reject({code: 400, body: {'error': "Invalid data inside zip file"}});
+                            let results = temp['result'];
+                            let clean_input_keys =
+                                [
+                                    'Subject',
+                                    'Course',
+                                    'Avg',
+                                    'Professor',
+                                    'Title',
+                                    'Pass',
+                                    'Fail',
+                                    'Audit',
+                                    'id'
+                                ];
+                            let clean_output_keys =
+                                [
+                                    'courses_dept',
+                                    'courses_id',
+                                    'courses_avg',
+                                    'courses_instructor',
+                                    'courses_title',
+                                    'courses_pass',
+                                    'courses_fail',
+                                    'courses_audit',
+                                    'courses_uuid'
+                                ];
+
+                            for (let r of results) {
+                                let newo: any = {};
+                                for (let i: number = 0; i < clean_input_keys.length; i++) {
+                                    newo[clean_output_keys[i]] = r[clean_input_keys[i]];
+                                }
+                                processed_results.push(newo);
+                            }
+
+                        }
+                        if (processed_results.length === 0)
+                            return reject({code: 400, body: {'error': "Nothing to write"}});
+                        if (fs.existsSync("./data")) {
+                            if (fs.existsSync("./data/" + id + ".dat")) {
+                                console.log("rewriting...");
+                                fs.writeFileSync("./data/" + id + ".dat", JSON.stringify(processed_results));
+                                return resolve({code: 201, body: {}});
+                            }
+                            else {
+                                let path: string = "./data/" + id + ".dat";
+                                fs.writeFileSync(path, JSON.stringify(processed_results));
+                                return resolve({code: 204, body: {}});
+                            }
                         }
                         else {
+                            fs.mkdirSync("./data");
                             let path: string = "./data/" + id + ".dat";
                             fs.writeFileSync(path, JSON.stringify(processed_results));
                             return resolve({code: 204, body: {}});
                         }
-                    }
-                    else {
-                        fs.mkdirSync("./data");
-                        let path: string = "./data/" + id + ".dat";
-                        fs.writeFileSync(path, JSON.stringify(processed_results));
-                        return resolve({code: 204, body: {}});
-                    }
-                }).catch(function (err) {
-                    return reject({code: 400, body: {"error": err.toString()}});
-                });
+                    }).catch(function (err) {
+                        return reject({code: 400, body: {"error": err.toString()}});
+                    });
+                }
+                else
+                {
+                    let proList: Promise<string>[] = [];
+                    let keys = Object.keys(data);
+                    let objkeys = Object.keys(data[keys[0]]);
+                    let rooms: any = [];
+                    let address:any = [];
+                    let building_list:any[] = [];
+                    zip.file("index.htm").async("string").then(function (data:any){
+                        let parsed = parse5.parse(data);
+                        index_tree_helper(parsed, building_list);
+                        for (let i = 1; i < objkeys.length; i++)
+                        {
+                            let file = zip.file(objkeys[i]);
+                            if(file && building_list.indexOf(objkeys[i].split("campus/discover/buildings-and-classrooms/")[1])>-1)
+                            {
+                                proList.push(file.async("string"));
+                            }
+                        }
+                        Promise.all(proList).then(strings => {
+
+                            for(let s of strings)
+                            {
+                                let tree = parse5.parse(s);
+                                building_tree_helper(tree, rooms, address);
+                            }
+                            return resolve({code: 201, body: {}});
+                        }).catch(function (err) {
+                            return reject({code: 400, body: {"error": err.toString()}});
+                        });
+                    }).catch(function (err: any){
+                        return reject({code: 400, body: {"error": err.toString()}});
+                    });
+                }
 
             }).catch(function (err: any) {
                 return reject({code: 400, body: {"error": err.toString()}});
             });
         });
     }
+
 
     removeDataset(id: string): Promise<InsightResponse> {
         return new Promise<InsightResponse>(function (resolve, reject) {
@@ -190,6 +230,171 @@ export default class InsightFacade implements IInsightFacade {
             return resolve({code: 200, body: re});
         });
     }
+}
+
+function index_tree_helper(node: any, list: any)
+{
+    //console.log("hi");
+    let nodeKeys = Object.keys(node);
+    if(nodeKeys.indexOf('attrs')>-1)
+    {
+        let attrs = node.attrs;
+        for(let at of attrs)
+        {
+            if(at.name == 'class'&&at.value == 'views-field views-field-field-building-code')
+            {
+                list.push(node.childNodes[0].value.split('\n')[1].replace(/ /g,''));
+            }
+        }
+    }
+    if(nodeKeys.indexOf('childNodes')>-1)
+    {
+        let children = node.childNodes;
+        for(let child of children)
+        {
+            index_tree_helper(child,list);
+        }
+    }
+    //return true;
+}
+
+function building_tree_helper(node: any, rooms:any, address_list:any)
+{
+    let key_list = [
+        "views-field views-field-field-room-number",
+        "views-field views-field-field-room-capacity",
+        "views-field views-field-field-room-furniture",
+        "views-field views-field-field-room-type",
+    ];
+    let nodeKeys = Object.keys(node);
+    if(nodeKeys.indexOf('attrs')>-1)
+    {
+        let attrs = node.attrs;
+        for(let at of attrs)
+        {
+            let room:string;
+            let shortname:string;
+            let fullname:string;
+            let address:string;
+            let href:string;
+            let number:string;
+            let room_name:string;
+            let seats:number;
+            let type:string;
+            let furniture:string;
+            if(at.name === "id" && at.value === "building-info")
+            {
+                let kids = node.childNodes;
+                let values = [];
+                for(let kid of kids)
+                {
+                    if(typeof kid.attrs !== 'undefined' && kid.attrs.length === 0)
+                    {
+                        if(typeof kid.childNodes !== 'undefined')
+                        {
+                            for (let a of kid.childNodes[0].attrs)
+                            {
+                                if (a.name === 'class' && a.value === 'field-content')
+                                {
+                                    let target_node = kid.childNodes[0].childNodes[0];
+                                    if(typeof target_node !== 'undefined' && typeof target_node.value !== 'undefined')
+                                        values.push(target_node.value);
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if(typeof kid.childNodes !== 'undefined')
+                        {
+                            for (let a of kid.attrs)
+                            {
+                                if (a.name === 'class' && a.value === 'building-field')
+                                {
+                                    for (let a of kid.childNodes[0].attrs)
+                                    {
+                                        if (a.name === 'class' && a.value === 'field-content')
+                                        {
+                                            let target_node = kid.childNodes[0].childNodes[0];
+                                            if(typeof target_node !== 'undefined' && typeof target_node.value !== 'undefined')
+                                                values.push(target_node.value);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                fullname = values[0];
+                address = values[1];
+                address_list.push(address);
+            }
+            else if(at.name === "class")
+            {
+
+                switch (at.value)
+                {
+                    case key_list[0]:
+                        if(typeof node.childNodes !== 'undefined')
+                        {
+                            for(let kid of node.childNodes)
+                            {
+                                if(typeof kid.childNodes !== 'undefined' && kid.attrs.length != 0)
+                                {
+                                    for(let att of kid.attrs)
+                                    {
+                                        if(att.name == "href")
+                                            href = att.value;
+                                    }
+                                    room_name = href.split("/room/")[1];
+                                    number = room_name.split("-")[1];
+                                    shortname = room_name.split("-")[0];
+                                }
+                            }
+                        }
+                        break;
+                    case key_list[1]:
+                        if(typeof node.childNodes !== 'undefined')
+                        {
+                            if(node.childNodes[0].value.replace( /^\D+/g, '') !== '')
+                            {
+                                seats = node.childNodes[0].value.replace(/^\D+/g, '');
+                            }
+                        }
+                        break;
+                    case key_list[2]:
+                        if(typeof node.childNodes !== 'undefined')
+                        {
+                            if(node.childNodes[0].value.replace(/\r?\n|\r/g, '').trim() != 'Furniture type')
+                            {
+                                furniture = node.childNodes[0].value.replace(/\r?\n|\r/g, '').trim();
+                            }
+                        }
+                        break;
+                    case key_list[3]:
+                        if(typeof node.childNodes !== 'undefined')
+                        {
+                            if(node.childNodes[0].value.replace(/\r?\n|\r/g, '').trim() != 'Room type')
+                            {
+                                type = node.childNodes[0].value.replace(/\r?\n|\r/g, '').trim();
+                                //console.log(type);
+                            }
+                        }
+                        break;
+                }
+            }
+            room = 
+        }
+    }
+    if(nodeKeys.indexOf('childNodes')>-1)
+    {
+        let children = node.childNodes;
+        for(let child of children)
+        {
+            building_tree_helper(child,rooms,address_list);
+        }
+    }
+    //return true;
 }
 
 function intersect(a:any,b:any) {
